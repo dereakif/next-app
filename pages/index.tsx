d5240167816e2e09194941dd97613d79e142a219
Akif <dereakif@gmail.com>
2022-02-02 00:31:22 +0300

    send: query with created client

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,17 @@
-import { NextPage } from "next";
 import Link from "next/link";
 import dbConnect from "../lib/mongodb";
+import { gql } from "@apollo/client";
+import apolloClient from "../lib/apollo";
 
-const IndexPage: NextPage = (props) => {
+interface IndexPageProps {
+  helloQuery?: string;
+}
+const IndexPage = (props: IndexPageProps) => {
+  const { helloQuery } = props;
   console.log({ props });
   return (
     <>
+      {helloQuery.trim() && <h1>{helloQuery}</h1>}
       <h2>hello world</h2>
       <Link href="/about">go to about page</Link>
     </>
@@ -16,8 +22,15 @@ export default IndexPage;
 export async function getServerSideProps() {
   try {
     await dbConnect();
+    const { data } = await apolloClient.query({
+      query: gql`
+        query hello {
+          hello
+        }
+      `,
+    });
     return {
-      props: { isConnected: true },
+      props: { isConnected: true, helloQuery: data.hello },
     };
   } catch (e) {
     console.error(e);
